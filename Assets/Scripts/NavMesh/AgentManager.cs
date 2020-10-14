da1a40392c379d1ce877aefb7663795f04fe8f15
TristanMoe <Trilleo@hotmail.com>
2020-10-14 10:45:04 +0200

    Updated stuff

---
+++ Assets/Scripts/NavMesh/AgentManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    // Requires GameObject in View. 
+    GameObject[] agents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find all agents in scene.
+        agents = GameObject.FindGameObjectsWithTag("AI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move all agents to position.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Sends raycast from mouse. 
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+            {
+                foreach (var agent in agents)
+                {
+                    agent.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NavMesh/ChasePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.NavMesh
+{
+    public class ChasePlayer : MonoBehaviour
+    {
+        GameObject player;
+        NavMeshAgent agent;
+
+        private void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+            agent = this.GetComponent<NavMeshAgent>(); 
+        }
+
+        private void Update()
+        {
+            agent.SetDestination(player.transform.position);  
+        }
+    }
+}
